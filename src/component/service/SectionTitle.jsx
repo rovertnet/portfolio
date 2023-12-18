7fff71f65431e9abc205ed3e0ab6363be370b602
Robert Matundu <matundukabamba@gmail.com>
2023-12-18 23:43:38 +0100

    une autre modif

--- src/component/service/SectionTitle.jsx
+++ src/component/service/SectionTitle.jsx
@@ -1,8 +1,12 @@
+import React from "react"
 
 function SectionTitle({ children, id }) {
   return (
     <div>
-        <h1 id={id && id} className=" text-3xl font-bold mb-5 text-blue-300 dark:text-blue-300">
+        <h1 
+            id={id && id} 
+            className=" text-3xl font-bold mb-5 text-blue-300 dark:text-blue-300"
+        >
             {children}
         </h1>
     </div>
