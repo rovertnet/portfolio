9ad8f8b6a7d9bd74b4eb003d0209e7df98f8eb78
Robert Matundu <matundukabamba@gmail.com>
2024-02-11 17:24:17 +0100

    update

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -1,12 +1,15 @@
-
-
+import SectionTitle from "../service/SectionTitle";
 
 function Skills() {
   return (
     <>
       <div className="md:px-8 px-6">
+        <SectionTitle>Compétences</SectionTitle>
         <div className="flex justify-center items-center gap-14">
-          <div className=""></div>
+          <div className="">
+            <h2 className="">Front End</h2>
+            <div className=""></div>
+          </div>
           <div className=""></div>
         </div>
       </div>
