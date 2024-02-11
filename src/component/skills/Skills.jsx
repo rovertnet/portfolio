6e416fc2968e78950a2977d2d6cb9b1597f48b68
Robert Matundu <matundukabamba@gmail.com>
2024-02-11 17:07:03 +0100

    update

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -5,7 +5,10 @@ function Skills() {
   return (
     <>
       <div className="md:px-8 px-6">
-        <div className="flex justify-center items-center gap-14"></div>
+        <div className="flex justify-center items-center gap-14">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
       </div>
     </>
   );
