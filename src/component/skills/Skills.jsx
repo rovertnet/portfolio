ad46ee55e8b1aa824db603d6fff1044a3ce948fb
Robert Matundu <matundukabamba@gmail.com>
2024-02-11 16:40:02 +0100

    update

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -4,9 +4,11 @@
 function Skills() {
   return (
     <>
-      <div className=""></div>
+      <div className="md:px-8 px-6">
+        <div className="flex justify-center items-center gap-14"></div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Skills
