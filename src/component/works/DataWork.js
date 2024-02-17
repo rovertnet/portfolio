016e823ed8c2feef10ac5382fab61329201fbf20
Robert Matundu <matundukabamba@gmail.com>
2024-02-17 16:53:46 +0100

    update

--- src/component/works/DataWork.js
+++ src/component/works/DataWork.js
@@ -1,22 +1,22 @@
 export default [
-    {
-        titre :'Ladingpage, tech',
-        imageUrl : '../../public/p1.jpg',
-        tech : ['HTML', 'Tailwindcss', 'React js'],
-    },
-    {
-        titre :'Site vitrine',
-        imageUrl : '../../public/p2.jpg',
-        tech : ['HTML', 'Tailwindcss', 'React js vite'],
-    },
-    {
-        titre :'site reserve',
-        imageUrl : '../../public/p3.jpg',
-        tech : ['HTML', 'Tailwindcss', 'React js vite'],
-    },
-    {
-        titre :'Portfolio',
-        imageUrl : '../../public/p4.jpg',
-        tech : ['HTML', 'Tailwindcss', 'React js vite'],
-    },
-]
+  {
+    titre: "Ladingpage, tech",
+    imageUrl: "https://portfolio-omega-two-26.vercel.app/p1.jpg",
+    tech: ["HTML", "Tailwindcss", "React js"],
+  },
+  {
+    titre: "Site vitrine",
+    imageUrl: "https://portfolio-omega-two-26.vercel.app/p2.jpg",
+    tech: ["HTML", "Tailwindcss", "React js vite"],
+  },
+  {
+    titre: "site reserve",
+    imageUrl: "https://portfolio-omega-two-26.vercel.app/p3.jpg",
+    tech: ["HTML", "Tailwindcss", "React js vite"],
+  },
+  {
+    titre: "Portfolio",
+    imageUrl: "https://portfolio-omega-two-26.vercel.app/p4.jpg",
+    tech: ["HTML", "Tailwindcss", "React js vite"],
+  },
+];
