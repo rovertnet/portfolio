b37969cae9fbb1efb38cac95874367f7f82683f3
Robert Matundu <matundukabamba@gmail.com>
2024-02-18 21:03:43 +0100

    update

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
   return (
     <div
       id="about"
-      className=" flex flex-col md:flex-row items-center justify-center gap-5 md:gap-10 py-6"
+      className=" flex flex-col md:flex-row items-center justify-center gap-5 md:gap-3 py-6"
     >
       <div className="w-full md:w-6/6">
         <SectionTitle>A propos</SectionTitle>
@@ -16,7 +16,7 @@ function About() {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.7 }}
-          className=" text-lg text-gray-600 dark:text-slate-300 md:px-8 px-6 text-justify"
+          className=" text-lg text-gray-600 dark:text-slate-300 md:px-8 px-6 text-justify max-w-4xl"
         >
           En tant que professionnel du développement web et de l'ingénierie
           logicielle, ma mission principale est de concevoir et mettre en œuvre
